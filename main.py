26fca6799aabe84e6d08a24f38f34e1984509402
Kyle Dickerson <kyle.dickerson@gmail.com>
2018-03-11 17:40:01 -0700

    Implement mocking high-level data responses. Have to use pickle not json due to json-incompatible data types.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from home_board import weather, calendar, special_events, compositor, util
 import datetime
-import pytz
 import os
+import pickle
+import pytz
 import sys
 
 TIME_ZONE = "America/Los_Angeles"
+SAVE_MOCK = False
+MOCK_CALENDAR = False
+MOCK_WEATHER = False
+MOCK_SPECIAL_EVENTS = False
+MOCK_CALENDAR_FILE = 'mock_data/mock_calendar_data.pickle'
+MOCK_WEATHER_FILE = 'mock_data/mock_weather_data.pickle'
+MOCK_SPECIAL_EVENTS_FILE = 'mock_data/mock_special_events_data.pickle'
 
 def _dst_start_end(tz_aware_when):
     dst_start_utc, dst_end_utc = [dt for dt in tz_aware_when.tzinfo._utc_transition_times if dt.year == tz_aware_when.year]
@@ -16,7 +24,14 @@ def _dst_start_end(tz_aware_when):
 
 def fetch_calendar(context, days):
     try:
-        calender_events = calendar.fetch(context['now'])
+        if MOCK_CALENDAR:
+            with open(util.local_file(MOCK_CALENDAR_FILE), 'rb') as mock_data:
+                calender_events = pickle.load(mock_data)
+        else:
+            calender_events = calendar.fetch(context['now'])
+            if SAVE_MOCK:
+                with open(util.local_file(MOCK_CALENDAR_FILE), 'wb') as mock_data:
+                    pickle.dump(calender_events, mock_data)
         for day in days:
             context[day]['events'] = calender_events[day]
     except Exception as ex:
@@ -25,7 +40,14 @@ def fetch_calendar(context, days):
 
 def fetch_weather(context, days):
     try:
-        conditions = weather.fetch() # weather can only fetch conditions and forecast for "now"
+        if MOCK_WEATHER:
+            with open(util.local_file(MOCK_WEATHER_FILE), 'rb') as mock_data:
+                conditions = pickle.load(mock_data)
+        else:
+            conditions = weather.fetch() # weather can only fetch conditions and forecast for "now"
+            if SAVE_MOCK:
+                with open(util.local_file(MOCK_WEATHER_FILE), 'wb') as mock_data:
+                    pickle.dump(conditions, mock_data)
         context['today']['conditions'] = conditions['current']
         for day in days:
             context[day]['forecast'] = conditions['forecast'][day]
@@ -35,7 +57,14 @@ def fetch_weather(context, days):
 
 def fetch_special_events(context, days):
     try:
-        special_event = special_events.fetch(context['today']['date'])
+        if MOCK_SPECIAL_EVENTS:
+            with open(util.local_file(MOCK_SPECIAL_EVENTS_FILE), 'rb') as mock_data:
+                special_event = pickle.load(mock_data)
+        else:
+            special_event = special_events.fetch(context['today']['date'])
+            if SAVE_MOCK:
+                with open(util.local_file(MOCK_SPECIAL_EVENTS_FILE), 'wb') as mock_data:
+                    pickle.dump(special_event, mock_data)
         for day in days:
             context[day]['special_event'] = special_event[day]
             if special_event[day] and day != 'today':
