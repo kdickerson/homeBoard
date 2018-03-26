5d2d8861a8e0eb01be7cccd55eeacd48dcac280b
Kyle Dickerson <kyle.dickerson@gmail.com>
2018-03-26 16:26:20 -0700

    Documentation comment about Special Event fields.

--- home_board/special_events.py
+++ home_board/special_events.py
@@ -1,6 +1,21 @@
 # Fetch special event info (Birthdays, Holidays, Anniversaries, etc. with image)
 import datetime
 
+'''
+    Entry fields:
+    year [optional]: year of event
+    month: month of event
+    day: day of event
+    header [optional]: Will be used as the header of the event entry when displayed on calendar
+    title [optional]: Will be used as body of the event entry when displayed on calendar
+    msg [optional]: Will be the special banner message displayed on the defined date
+    icon [optional]: Will be displayed with the special banner message on the defined date
+
+    If title is defined, event (header/title) will appear on future dates in the calendar
+        If msg is defined, event will not appear on "today", instead the special banner message is displayed
+        If msg is not defined, event will appear on "today"
+'''
+
 EVENTS = [
     {'month': 3, 'day': 17, 'header': 'Birthday', 'title': 'Corinne', 'msg': 'Happy Birthday, Corinne!', 'icon': 'birthday'},
     {'month': 6, 'day': 9, 'header': 'Birthday', 'title': 'Kyle', 'msg': 'Happy Birthday, Kyle!', 'icon': 'birthday'},
